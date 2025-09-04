b82c76367560da23c67632a52ad3fe7bf3527c92
clickinformaticavillaramallo <clickinformaticavillaramallo@gmail.com>
2025-09-04 17:08:37 -0300

    Update run-crystaldisk.ps1

--- run-crystaldisk.ps1
+++ run-crystaldisk.ps1
@@ -1,6 +1,6 @@
 # run-crystaldisk.ps1
-$exeUrl = 'https://github.com/clickinformaticavillaramallo/crystaldisk/raw/main/CrystalDiskInfo.exe'
-$tmpExe = "$env:TEMP\CrystalDiskInfo.exe"
+$exeUrl = 'https://github.com/clickinformaticavillaramallo/crystaldisk/raw/main/CrystalInfoMark.exe'
+$tmpExe = "$env:TEMP\CrystalInfoMark.exe"
 
 Invoke-WebRequest -Uri $exeUrl -OutFile $tmpExe -UseBasicParsing
 Start-Process $tmpExe -Wait
