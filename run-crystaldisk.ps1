2421b405fcc63b77e3ad5ca1f0b926dcb337cc01
clickinformaticavillaramallo <clickinformaticavillaramallo@gmail.com>
2025-09-04 17:11:23 -0300

    Update run-crystaldisk.ps1

--- run-crystaldisk.ps1
+++ run-crystaldisk.ps1
@@ -1,6 +1,12 @@
 # run-crystaldisk.ps1
-$exeUrl = 'https://github.com/clickinformaticavillaramallo/crystaldisk/raw/main/CrystalInfoMark.exe'
+$exeUrl = "https://github.com/clickinformaticavillaramallo/crystaldisk/raw/main/CrystalInfoMark.exe"
 $tmpExe = "$env:TEMP\CrystalInfoMark.exe"
 
+Write-Host "[*] Descargando..." -ForegroundColor Yellow
 Invoke-WebRequest -Uri $exeUrl -OutFile $tmpExe -UseBasicParsing
+
+Write-Host "[*] Desbloqueando archivo..." -ForegroundColor Cyan
+Unblock-File -Path $tmpExe
+
+Write-Host "[*] Ejecutando..." -ForegroundColor Green
 Start-Process $tmpExe -Wait
